174530e6fe8be3e490d9960c797796ef81b35957
悠也 <@MURATA-LPC>
2017-01-27 08:26:54 +0900

    notePC commit

--- PlatooningABS/src/prop/ABSSettings.java
+++ PlatooningABS/src/prop/ABSSettings.java
@@ -12,6 +12,6 @@ import prop.json.ABSJSONReader;
  * @author 悠也
  */
 public interface ABSSettings {
-    public static String settingFileName = "platooning_abs_param.json";
+    public static String settingFileName = "platooning_abs_param_demo.json";
     public static ABSJSONReader json = ABSJSONReader.getInstance();
 }
